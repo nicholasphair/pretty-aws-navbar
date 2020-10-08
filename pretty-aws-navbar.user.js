d0ec59c4e5508de4859c6ba979a385fd0eda6387
Nick Phair <nickphair@gmail.com>
2020-10-07 23:05:33 -0400

    initial v2 commit for redesigned aws console

--- pretty-aws-navbar.user.js
+++ pretty-aws-navbar.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name pretty-aws-navbar
 // @description Prettify the AWS Console's Navigation Bar.
+// @include https://console.aws.amazon.com/console/home?region=us-east-1#
 // @include https://console.aws.amazon.com/*/home*
 // @include https://*.console.aws.amazon.com/*/home*
 // @include https://*.console.aws.amazon.com/GetResource/Console.html*
@@ -8,19 +9,21 @@
 // @include https://*.console.amazonaws-us-gov.com/*/home*
 // @include https://*.console.amazonaws-us-gov.com/GetResource/Console.html*
 // @exclude https://console.aws.amazon.com/s3/home*
-// @version 1.1 
+// @version 1.1
 //  ==/UserScript==
 
 
 /* ------------------- */
 /* YOUR ACCOUNTS HERE! */
 /* ------------------- */
-const accountNavBars = new Map([
-  ['foo', buildNavBar('CornflowerBlue')],
-  ['bar', buildNavBar('FireBrick')],
-  ['baz', buildNavBar('OliveDrab')],
+const accountColors = new Map([
+  ['nphair', 'FireBrick'],
+  ['bar', 'FireBrick'],
+  ['baz', 'OliveDrab'],
 ]);
 
+
+// TODO (nphair): Look these up in parameter store.
 const regions = new Map([
   ['Bahrain', 'me-south-1'],
   ['Central', 'ca-central-1'],
@@ -51,38 +54,47 @@ function prettify() {
 }
 
 function prettifyNavBar() {
-  let accountElement = document.querySelector('#nav-usernameMenu > div.nav-elt-label');
-  let account = accountElement.innerHTML.split('@')[1].trim();
-  if(!isEmpty(account) && accountNavBars.has(account)) {
-    let styleSheet = document.createElement('style');
-    styleSheet.setAttribute('type', 'text/css');
-    styleSheet.textContent = accountNavBars.get(account);
-    document.head.appendChild(styleSheet);
+  let accountElement = document.querySelector('[data-testid="awsc-nav-account-menu-button"]');
+  let account = accountElement.innerText.split('@')[1].trim();
+  if(!isEmpty(account) && accountColors.has(account)) {
+    let color = accountColors.get(account);
+    document.querySelectorAll('[data-testid^="awsc"]').forEach(element => {
+      console.log(element);
+      element.style.background = color;
+      element.style.backgroundColor = color;
+
+      // TODO (nphair): Bring back gradient.
+      //element.style.background = `linear-gradient(to bottom, ${color} 0px, #000 100%)`;
+      //element.style.backgroundColor = `linear-gradient(to bottom, ${color} 0px, #000 100%)`;
+    });
+
+    // Dividers.
+    // TODO (nphair): Dynamically calculate complementary color for dividers.
+    let dividerColor = color
+    document.querySelectorAll('[role="menu"] hr').forEach(element => {
+      element.style.backgroundColor = dividerColor;
+      element.style.border = dividerColor;
+
+    });
+     document.querySelectorAll('[role="menuitem"], [data-testid^="awsc"]').forEach(element => {
+      element.style.borderColor = dividerColor;
+    });
+    document.querySelectorAll('#awsc-navigation__more-menu--list > [role="menuitem"] > div > div').forEach(element => {
+      element.style.background = dividerColor;
+    });
   } else {
-    console.log('could not determine the account or the account styling.');
+    console.error('could not determine the account or the account styling.');
   }
 }
 
-function buildNavBar(color) {
-  let NavBarElements = `
-    body #awsgnav #nav-menubar,
-    body #awsgnav #nav-menubar .nav-menu,
-    #nav-menu-right {
-      background-color: transparent;
-    }`;
-  let NavBarBackground = `#consoleNavHeader {
-    background: linear-gradient(to bottom, ${color} 0px, #000 100%);
-  }`;
-  return `${NavBarBackground} ${NavBarElements}`;
-}
 
 function prettifyRegion() {
-  let regionElement = document.querySelector('#nav-regionMenu > div.nav-elt-label');
-  let region = regionElement.innerHTML;
+  let regionElement = document.querySelector('[data-testid="awsc-nav-regions-menu-button"]');
+  let region = regionElement.innerText;
   if(!isEmpty(region) && regions.has(region)) {
-    regionElement.innerHTML = `${region}: ${regions.get(region)}`;
+    regionElement.innerText = `${region}: ${regions.get(region)}`;
   } else {
-    console.log('could not determine the region code or the region name.');
+    console.error('could not determine the region code or the region name.');
   }
 }
 
@@ -90,4 +102,4 @@ function isEmpty(str) {
   return (!str || 0 === str.length);
 }
 
-setTimeout(prettify, 300);
+setTimeout(prettify, 2500);
